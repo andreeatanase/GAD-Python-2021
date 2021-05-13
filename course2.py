f608c7d8f0035071aca78360e58ec8bb82792737
andreeatanase <andy.tanase@yahoo.com>
2021-05-13 19:35:33 +0300

    Python basics

--- course2.py
+++ course2.py
@@ -38,12 +38,12 @@ print('my_int_from_float', my_int_from_float, type(my_int_from_float))
 print('my_float_from_int', my_float_from_int, type(my_float_from_int))
 
 # Python None is...
-# and or not , is, is not - operatori
+# and or not , is, is not - operators
 # print(7 is 7)
-print([1, 2] is [1, 2])  # se afla la locatii de memorie diferite in coada si de aceea este fals
+print([1, 2] is [1, 2])  # are found at different memory locations
 
-# in si not in - operatori de apartenenta
-# === - egalitate completa (in Javascript) verifica atat tipul de date cat si valoarea
+# in si not in - belonging operators
+# === - complete equality (in Javascript) checks the type and the value
 # 7=='7' va da true, dar 7==='7' va da false
 # str(7)=='7' sau 7==int(7)
 
@@ -51,7 +51,7 @@ print([1, 2] is [1, 2])  # se afla la locatii de memorie diferite in coada si de
 # print(a == b)  # false because they are different types
 a = 7
 b = 7
-print(a is b, id(a), id(b))  # afiseaza adresa in memorie
+print(a is b, id(a), id(b))  # prints the address from the memory
 
 c = [1, 2]
 d = [1, 2]
@@ -60,4 +60,10 @@ print(c is d, id(c), id(d))
 # ' ' == " "
 escape_character = 'hhfidjsif \' fhddjhd'  # ' - can be used as a character within the string
 
+print(r'linia 1\nlinia2\nlinia2\n')  # rawstring
+print(R'linia 1\nlinia2\nlinia2\n')
 
+price = 10.00
+name = 'Hamburger'
+msg = 'Top product: Burger costs only $20.00'
+print(msg)
